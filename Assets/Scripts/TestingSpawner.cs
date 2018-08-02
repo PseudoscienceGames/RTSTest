fdcaa079c2fc9e9317953b447c7f0db4e9278cda
PseudoscienceGames <quade.81@gmail.com>
2018-08-02 16:33:02 -0500

    Initial commit

---
+++ Assets/Scripts/TestingSpawner.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingSpawner : MonoBehaviour
+{
+	public void SpawnPlayerUnit()
+	{
+		Instantiate(Resources.Load("PlayerUnit"));
+	}
+}
